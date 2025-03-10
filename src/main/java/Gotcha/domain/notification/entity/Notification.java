351201ed43a5dbf0c1644fb84bed998959ee4e59
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-10 15:10:46 +0900

    feat: Notification Entity, Repository 설정

--- src/main/java/Gotcha/domain/notification/entity/Notification.java
+++ src/main/java/Gotcha/domain/notification/entity/Notification.java
@@ -1,4 +1,44 @@
 package Gotcha.domain.notification.entity;
 
-public class Notification{
+import Gotcha.common.entity.BaseTimeEntity;
+import Gotcha.domain.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+
+    private Boolean isFixed;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User writer;
+
+    @Builder
+    public Notification(String title, String content, Boolean isFixed){
+        this.title = title;
+        this.content = content;
+        this.isFixed = isFixed;
+    }
 }
