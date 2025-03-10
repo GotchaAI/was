6f8d4ab82c606d8ecfcf3b3d101ae45c3b5e9cf2
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-10 15:06:55 +0900

    feat: Answer Entity, Repository 설정

--- src/main/java/Gotcha/domain/inquiry/entity/Answer.java
+++ src/main/java/Gotcha/domain/inquiry/entity/Answer.java
@@ -1,4 +1,41 @@
 package Gotcha.domain.inquiry.entity;
 
-public class Answer {
+import Gotcha.common.entity.BaseTimeEntity;
+import Gotcha.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Answer extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User writer;
+
+    @OneToOne(mappedBy = "answer", fetch = FetchType.LAZY)
+    private Inquiry inquiry;
+
+    @Builder
+    public Answer(String content){
+        this.content = content;
+    }
 }
