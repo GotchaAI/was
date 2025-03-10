9093b55e32d840ddc78696ab1db564bfe0326e12
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-10 15:05:24 +0900

    feat: BaseTimeEntity 작성

---
+++ src/main/java/Gotcha/common/entity/BaseTimeEntity.java
@@ -0,0 +1,28 @@
+package Gotcha.common.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @JsonIgnore
+    @Column(name = "created_At", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @JsonIgnore
+    @Column(name = "modified_At")
+    private LocalDateTime modifiedAt;
+}
