3b7eef8c8e99b1d937057f67bde74af49f9b2d54
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-15 14:08:16 +0900

    feat: RefreshTokenService 작성

---
+++ src/main/java/Gotcha/common/jwt/RefreshTokenService.java
@@ -0,0 +1,35 @@
+package Gotcha.common.jwt;
+
+import Gotcha.common.util.RedisUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import static Gotcha.common.redis.RedisProperties.REFRESH_TOKEN_KEY_PREFIX;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+    private final RedisUtil redisUtil;
+    private final TokenProvider tokenProvider;
+
+    @Value("${token.refresh.in-redis}")
+    private long REFRESH_EXPIRATION;
+
+    public void saveRefreshToken(String email, String refreshToken) {
+        String key = REFRESH_TOKEN_KEY_PREFIX + email;
+        redisUtil.setData(key, refreshToken);
+        redisUtil.setDataExpire(key, REFRESH_EXPIRATION);
+    }
+
+    public void deleteRefreshToken(String refreshToken) {
+        String email = tokenProvider.getEmail(refreshToken);
+        redisUtil.deleteData(email);
+    }
+
+    public boolean existedRefreshToken(String email) {
+        String key = REFRESH_TOKEN_KEY_PREFIX + email;
+
+        return redisUtil.existed(key);
+    }
+}
