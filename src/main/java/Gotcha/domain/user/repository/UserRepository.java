8519383576a4bd3edde75696b102c099fdd324bb
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-15 14:01:44 +0900

    feat: UserDetailsService 구현

--- src/main/java/Gotcha/domain/user/repository/UserRepository.java
+++ src/main/java/Gotcha/domain/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package Gotcha.domain.user.repository;
 import Gotcha.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
