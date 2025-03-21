837acbb2b9771bc642c83359356166880c195d51
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-21 16:26:22 +0900

    feat: CacheConfig 설정

--- src/main/java/Gotcha/common/jwt/UserDetailsServiceImpl.java
+++ src/main/java/Gotcha/common/jwt/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package Gotcha.common.jwt;
 import Gotcha.domain.user.entity.User;
 import Gotcha.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,6 +15,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Cacheable(value = "users", key = "#username")
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found : " + username));
