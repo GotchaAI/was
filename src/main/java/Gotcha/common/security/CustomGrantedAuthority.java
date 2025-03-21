5879290bd553a6cd2848dcd44d670bc21ec65313
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-21 16:45:57 +0900

    feat: 권한 인증 캐시 설정

---
+++ src/main/java/Gotcha/common/security/CustomGrantedAuthority.java
@@ -0,0 +1,23 @@
+package Gotcha.common.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.util.Assert;
+
+public class CustomGrantedAuthority implements GrantedAuthority {
+
+    private final String authority;
+
+    @JsonCreator
+    public CustomGrantedAuthority(@JsonProperty("authority") String role) {
+        Assert.hasText(role, "role은 비어있을 수 없습니다.");
+        this.authority = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+}
