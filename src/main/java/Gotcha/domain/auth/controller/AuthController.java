bdfdfdc77876e22bf195e9b2b19fb345ce8482cb
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-23 14:30:15 +0900

    feat: mail 컨트롤러 작성 및 dto 생성

--- src/main/java/Gotcha/domain/auth/controller/AuthController.java
+++ src/main/java/Gotcha/domain/auth/controller/AuthController.java
@@ -2,8 +2,10 @@ package Gotcha.domain.auth.controller;
 
 import Gotcha.common.exception.CustomException;
 import Gotcha.common.jwt.exception.JwtExceptionCode;
+import Gotcha.common.mail.MailCodeService;
 import Gotcha.common.util.CookieUtil;
 import Gotcha.domain.auth.api.AuthApi;
+import Gotcha.domain.auth.dto.EmailReq;
 import Gotcha.domain.auth.dto.SignInReq;
 import Gotcha.domain.auth.dto.SignUpReq;
 import Gotcha.domain.auth.dto.TokenDto;
@@ -13,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ import static Gotcha.common.jwt.JwtProperties.REFRESH_COOKIE_VALUE;
 public class AuthController implements AuthApi {
     private final AuthService authService;
     private final CookieUtil cookieUtil;
+    private final MailCodeService mailCodeService;
 
     @PostMapping("/sign-up")
     public ResponseEntity<?> signUp(@Valid @RequestBody SignUpReq signUpReq) {
@@ -53,6 +55,13 @@ public class AuthController implements AuthApi {
         TokenDto tokenDto = authService.reissueAccessToken(refreshToken);
 
         return createTokenRes(tokenDto);
+    }
+
+    @PostMapping("/email/send")
+    public ResponseEntity<?> sendEmail(@Valid @RequestBody EmailReq emailReq) {
+        // Todo : 이메일 중복 확인 로직 필요
+        mailCodeService.sendCodeToMail(emailReq.email());
+        return ResponseEntity.ok().build();
     }
 
     private ResponseEntity<?> createTokenRes(TokenDto tokenDto) {
