ad4ef6a2f28b78d6e95ca6b049adc6554a036c1b
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-23 14:53:30 +0900

    feat: 회원가입 시 이메일 인증 여부 확인 로직 추가

--- src/main/java/Gotcha/domain/auth/exception/AuthExceptionCode.java
+++ src/main/java/Gotcha/domain/auth/exception/AuthExceptionCode.java
@@ -5,10 +5,11 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
-public enum UserExceptionCode implements ExceptionCode {
+public enum AuthExceptionCode implements ExceptionCode {
 
     INVALID_USERNAME_AND_PASSWORD(HttpStatus.NOT_FOUND, "아이디 또는 비밀번호가 유효하지 않습니다."),
-    INVALID_USERID(HttpStatus.NOT_FOUND,"존재하지 않는 사용자입니다.");
+    INVALID_USERID(HttpStatus.NOT_FOUND,"존재하지 않는 사용자입니다."),
+    NOT_VERIFIED_EMAIL(HttpStatus.BAD_REQUEST, "이메일 인증이 완료되지 않았습니다."),;
 
     private final HttpStatus status;
     private final String message;
