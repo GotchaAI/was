dceeb6989f8be121031df3295fae1ceb829332f8
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-23 19:41:40 +0900

    feat: 닉네임 중복 검사 로직 수정

--- src/main/java/Gotcha/domain/user/service/UserService.java
+++ src/main/java/Gotcha/domain/user/service/UserService.java
@@ -1,27 +1,37 @@
 package Gotcha.domain.user.service;
 
 import Gotcha.common.exception.CustomException;
+import Gotcha.common.util.RedisUtil;
 import Gotcha.domain.user.exceptionCode.UserExceptionCode;
 import Gotcha.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static Gotcha.common.redis.RedisProperties.NICKNAME_VERIFY_KEY_PREFIX;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final RedisUtil redisUtil;
+
+    private static final long NICKNAME_VERIFY_EXPIRATION_TIME = 10 * 60;
 
     @Transactional(readOnly = true)
     public void checkNickname(String nickname) {
-        if(userRepository.existsByNickname(nickname)) {
+        if (userRepository.existsByNickname(nickname)) {
             throw new CustomException(UserExceptionCode.NICKNAME_EXIST);
         }
+
+        String verifiedKey = NICKNAME_VERIFY_KEY_PREFIX + nickname;
+        redisUtil.setData(verifiedKey, "true");
+        redisUtil.setDataExpire(verifiedKey, NICKNAME_VERIFY_EXPIRATION_TIME);
     }
 
     @Transactional(readOnly = true)
     public void checkEmail(String email) {
-        if(userRepository.existsByEmail(email)) {
+        if (userRepository.existsByEmail(email)) {
             throw new CustomException(UserExceptionCode.EMAIL_EXIST);
         }
     }
