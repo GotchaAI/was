c4ff7560205518f4713fe9d2b58ddab1c356fd7b
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-03-23 14:48:29 +0900

    feat: mail 인증 로직 작성

--- src/main/java/Gotcha/common/mail/MailCodeService.java
+++ src/main/java/Gotcha/common/mail/MailCodeService.java
@@ -4,16 +4,19 @@ import Gotcha.common.exception.CustomException;
 import Gotcha.common.exception.exceptionCode.GlobalExceptionCode;
 import Gotcha.common.mail.exception.MailExceptionCode;
 import Gotcha.common.util.RedisUtil;
+import Gotcha.domain.auth.dto.EmailCodeVerifyReq;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static Gotcha.common.redis.RedisProperties.CODE_EXPIRATION_TIME;
 import static Gotcha.common.redis.RedisProperties.CODE_KEY_PREFIX;
+import static Gotcha.common.redis.RedisProperties.EMAIL_VERIFY_KEY_PREFIX;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +25,7 @@ public class MailCodeService {
     private final RedisUtil redisUtil;
 
     private static final int RESEND_THRESHOLD_SECONDS = 4 * 60;
+    private static final long EMAIL_VERIFY_EXPIRATION_TIME = 10 * 60;
 
     public void sendCodeToMail(String email) {
         if (!checkRetryEmail(email)) {
@@ -56,5 +60,23 @@ public class MailCodeService {
         } catch (NoSuchAlgorithmException e) {
             throw new CustomException(GlobalExceptionCode.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    public void verifiedCode(EmailCodeVerifyReq emailCodeVerifyReq) {
+        String key = CODE_KEY_PREFIX + emailCodeVerifyReq.email();
+        Optional<String> storedCode = Optional.ofNullable((String) redisUtil.getData(key));
+
+        if (storedCode.isEmpty()) {
+            throw new CustomException(MailExceptionCode.CODE_EXPIRED);
+        }
+        if (!storedCode.get().equals(emailCodeVerifyReq.code())) {
+            throw new CustomException(MailExceptionCode.INVALID_CODE);
+        }
+
+        redisUtil.deleteData(key);
+
+        String verifiedKey = EMAIL_VERIFY_KEY_PREFIX + emailCodeVerifyReq.email();
+        redisUtil.setData(verifiedKey, "true");
+        redisUtil.setDataExpire(verifiedKey, EMAIL_VERIFY_EXPIRATION_TIME);
     }
 }
--- src/main/java/Gotcha/common/mail/exception/MailExceptionCode.java
+++ src/main/java/Gotcha/common/mail/exception/MailExceptionCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum MailExceptionCode implements ExceptionCode {
-    ALREADY_MAIL_REQUEST(HttpStatus.TOO_MANY_REQUESTS,"이미 메일을 요청하셨습니다.");
+    ALREADY_MAIL_REQUEST(HttpStatus.TOO_MANY_REQUESTS,"이미 메일을 요청하셨습니다."),
+    CODE_EXPIRED(HttpStatus.BAD_REQUEST,"인증번호가 만료되었습니다."),
+    INVALID_CODE(HttpStatus.BAD_REQUEST,"인증번호가 일치하지 않습니다.");
 
     private final HttpStatus status;
     private final String message;
