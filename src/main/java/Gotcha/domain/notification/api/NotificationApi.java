58c3180a2086a9eb618597045f6a8be4d2daab29
brothergiven <dung3740@gmail.com>
2025-03-27 19:52:24 +0900

    feat: 사용자 공지사항 swagger 작성

---
+++ src/main/java/Gotcha/domain/notification/api/NotificationApi.java
@@ -0,0 +1,116 @@
+package Gotcha.domain.notification.api;
+
+
+import Gotcha.domain.notification.dto.NotificationSortType;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Tag(name = "[공지사항 API]", description = "공지사항 관련 API")
+public interface NotificationApi {
+
+    @Operation(summary = "공지사항 목록", description = "공지사항 목록을 조회하는 API. Keyword로 검색 및 정렬 가능.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200", description = "공지사항 목록 조회 성공",
+                    content = @Content(mediaType = "application/json", examples = {
+                        @ExampleObject(value = """
+                                {
+                                    "totalPages": 1, // 전체 페이지 수
+                                    "totalElements": 3, // 전체 게시글 수
+                                    "first": true, // 첫 페이지인지
+                                    "last": true, // 마지막 페이지인지
+                                    "size": 10, // 한 페이지에 보여줄 아이템 수
+                                    "content": [
+                                        {
+                                            "notificationId": 2,
+                                            "title" : "걍 공지사항이다",
+                                            "createdAt": "2025-03-27T16:13:32",
+                                            "writer" : "묘묘"
+                                        }, 
+                                        {
+                                            "notificationId": 1,
+                                            "title" : "서버 점검 안내",
+                                            "createdAt": "2025-03-27T16:05:35",  
+                                            "writer": "루루"
+                                        },
+                                        {
+                                            "notificationId": 0,
+                                            "title": "이달의 인공지능 선정 결과",
+                                            "createdAt" : "2001-11-16T16:02:26",
+                                            "writer": "킹형준"
+                                        }
+                                    ],
+                                    "pageable": {
+                                        "pageNumber": 0,
+                                        "pageSize": 10,
+                                        "sort": {
+                                            "empty": false,
+                                            "unsorted": false,
+                                            "sorted": true
+                                        },
+                                        "offset": 0,
+                                        "unpaged": false,
+                                        "paged": true
+                                    },
+                                    "numberOfElements": 3, // 현재 페이지 아이템 수
+                                    "sort": {
+                                        "empty": false,
+                                        "sorted": true,
+                                        "unsorted": false
+                                    },
+                                    "number": 0, // 현재 페이지 번호
+                                    "empty": false // 빈 페이지인지                            
+                                }   
+                                """)
+                    })
+            )
+
+    })
+    ResponseEntity<?> getNotifications(@RequestParam(value = "keyword", required = false) String keyword,
+                                       @RequestParam(value = "page") Integer page,
+                                       @RequestParam(value = "sort") NotificationSortType sort);
+
+
+    @Operation(summary = "공지사항 조회", description = "공지사항 ID를 받아 해당 공지사항을 조회하는 API")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200", description = "공지사항 조회 성공",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                        "title": "걍 공지사항이다",
+                                        "content": "걍 공지사항이다 인마",
+                                        "createdAt": "2025-03-27T16:13:32",
+                                        "modifiedAt": "2025-11-16T16:13:32",
+                                        "writer": "묘묘",
+                                        "isFixed": true
+                                    }
+                                    """
+                            )
+                    })
+            ),
+            @ApiResponse(
+                    responseCode = "404", description = "존재하지 않는 공지사항",
+                    content = @Content(mediaType = "application/json", examples = {
+                            @ExampleObject(value = """
+                                    {
+                                        "status": "NOT_FOUND",
+                                        "message": "존재하지 않는 공지사항입니다."
+                                    }
+                                    """
+                            )
+                    })
+            )
+
+    })
+    ResponseEntity<?> getNotificationsById(@PathVariable(value = "notificationId") Long notificationId);
+
+
+}
