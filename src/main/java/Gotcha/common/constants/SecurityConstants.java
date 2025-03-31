ce6839d16736f8be7ebcaf6b89792ce494905d88
brothergiven <dung3740@gmail.com>
2025-03-31 11:01:13 +0900

    feat: 퍼블릭 엔드포인트 추가

--- src/main/java/Gotcha/common/constants/SecurityConstants.java
+++ src/main/java/Gotcha/common/constants/SecurityConstants.java
@@ -3,7 +3,8 @@ package Gotcha.common.constants;
 public class SecurityConstants {
     public static final String[] PUBLIC_ENDPOINTS = {
             "/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs/**",
-            "/webjars/**", "/error", "/api/v1/auth/**", "/api/v1/users/nickname-check"
+            "/webjars/**", "/error", "/api/v1/auth/**", "/api/v1/users/nickname-check",
+            "/api/v1/notifications/**"
     };
 
     public static final String[] ADMIN_ENDPOINTS = {"/api/v1/admin/**"};
