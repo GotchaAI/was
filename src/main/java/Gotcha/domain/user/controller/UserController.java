b2f96f2521a9369ed3ea441a3356456b979b8d2c
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-04-10 15:25:14 +0900

    feat: 사용자 정보 반환 로직 작성

--- src/main/java/Gotcha/domain/user/controller/UserController.java
+++ src/main/java/Gotcha/domain/user/controller/UserController.java
@@ -1,12 +1,17 @@
 package Gotcha.domain.user.controller;
 
 import Gotcha.common.api.SuccessRes;
+import Gotcha.common.jwt.auth.SecurityUserDetails;
 import Gotcha.domain.user.api.UserApi;
 import Gotcha.domain.user.dto.NicknameReq;
+import Gotcha.domain.user.dto.UserInfoRes;
 import Gotcha.domain.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +29,12 @@ public class UserController implements UserApi {
         userService.checkNickname(nicknameReq.nickname());
 
         return ResponseEntity.ok(SuccessRes.from("사용 가능한 닉네임입니다."));
+    }
+
+    @GetMapping("/me/main-info")
+    public ResponseEntity<?> getUserInfo(@AuthenticationPrincipal SecurityUserDetails userDetails){
+        UserInfoRes userInfoRes = userService.getUserInfo(userDetails.getId(), userDetails.getRole());
+
+        return ResponseEntity.status(HttpStatus.OK).body(userInfoRes);
     }
 }
