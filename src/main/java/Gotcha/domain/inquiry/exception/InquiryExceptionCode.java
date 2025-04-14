07136f2c2dbbfad73143935cc46ac2e523468f24
brothergiven <dung3740@gmail.com>
2025-04-15 00:24:14 +0900

    feat: 이미 작성된 답변 작성 못하도록 구현

--- src/main/java/Gotcha/domain/inquiry/exception/InquiryExceptionCode.java
+++ src/main/java/Gotcha/domain/inquiry/exception/InquiryExceptionCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 public enum InquiryExceptionCode implements ExceptionCode {
 
     INVALID_INQUIRYID(HttpStatus.NOT_FOUND, "존재하지 않는 QnA 입니다."),
-    UNAUTHORIZED_ACTION(HttpStatus.FORBIDDEN, "권한이 없습니다.");
+    UNAUTHORIZED_ACTION(HttpStatus.FORBIDDEN, "권한이 없습니다."),
+    ALREADY_SOLVED(HttpStatus.CONFLICT, "이미 답변이 작성되었습니다.");
 
     private final HttpStatus status;
     private final String message;
--- src/main/java/Gotcha/domain/inquiry/service/AnswerService.java
+++ src/main/java/Gotcha/domain/inquiry/service/AnswerService.java
@@ -1,9 +1,12 @@
 package Gotcha.domain.inquiry.service;
 
+import Gotcha.common.exception.CustomException;
 import Gotcha.domain.inquiry.dto.AnswerReq;
 import Gotcha.domain.inquiry.entity.Answer;
 import Gotcha.domain.inquiry.entity.Inquiry;
+import Gotcha.domain.inquiry.exception.InquiryExceptionCode;
 import Gotcha.domain.inquiry.repository.AnswerRepository;
+import Gotcha.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,9 +20,12 @@ public class AnswerService {
     private final InquiryService inquiryService;
 
     @Transactional
-    public void createAnswer(AnswerReq answerReq, Long inquiryId) {
+    public void createAnswer(AnswerReq answerReq, Long inquiryId, Long userId) {
+        User writer = inquiryService.getValidUser(userId);
         Inquiry inquiry = inquiryService.getValidInquiry(inquiryId);
-        Answer answer = answerReq.toEntity();
+        if(inquiry.getIsSolved())
+            throw new CustomException(InquiryExceptionCode.ALREADY_SOLVED);
+        Answer answer = answerReq.toEntity(writer);
         inquiry.solve(answer);
         answerRepository.save(answer);
     }
