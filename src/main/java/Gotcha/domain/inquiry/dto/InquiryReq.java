411da8516bac5641f13206d6fabdbf648f413368
brothergiven <dung3740@gmail.com>
2025-04-15 14:45:18 +0900

    docs: Req DTO Swagger 설명 추가

--- src/main/java/Gotcha/domain/inquiry/dto/InquiryReq.java
+++ src/main/java/Gotcha/domain/inquiry/dto/InquiryReq.java
@@ -2,13 +2,19 @@ package Gotcha.domain.inquiry.dto;
 
 import Gotcha.domain.inquiry.entity.Inquiry;
 import Gotcha.domain.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record InquiryReq(
+        @Schema(description = "QnA 제목", example = "게임이 너무 재밌는 것 같아요!")
         @NotBlank(message = "제목은 필수 입력 사항입니다.")
         String title,
+
+        @Schema(description = "QnA 질문 내용", example = "이 편지는 영국에서 최초로 시작되어 일년에 한바퀴를 돌면서 받는 사람에게 행운을 주었고 지금은 당신에게로 옮겨진 이 편지는 4일 안에 당신 곁을 ...더보기")
         @NotBlank(message = "내용은 필수 입력 사항입니다.")
         String content,
+
+        @Schema(description = "비공개 여부", example = "false", defaultValue = "false")
         Boolean isPrivate
 ) {
     public InquiryReq {
--- src/main/java/Gotcha/domain/notification/dto/NotificationReq.java
+++ src/main/java/Gotcha/domain/notification/dto/NotificationReq.java
@@ -2,12 +2,15 @@ package Gotcha.domain.notification.dto;
 
 import Gotcha.domain.notification.entity.Notification;
 import Gotcha.domain.user.entity.User;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record NotificationReq(
+        @Schema(description = "공지사항 제목", example = "걍 공지사항이다")
         @NotBlank(message = "제목은 필수 입력 사항입니다.")
         String title,
 
+        @Schema(description = "공지사항 내용", example = "걍 공지사항이다 인마")
         @NotBlank(message = "내용은 필수 입력 사항입니다.")
         String content
 ) {
