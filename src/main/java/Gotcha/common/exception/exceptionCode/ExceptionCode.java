668ce397f6ad0aa17b0c1d6272965171a8a383b5
이승현 <50231309+Uralauah@users.noreply.github.com>
2025-04-22 08:09:21 +0900

    refact: 예외 코드 수정 (#26)
    * refact: 에러 코드 추가
    
    * refact: 예외 별 에러 코드 추가
    
    * refact: 예외 dto 수정

--- src/main/java/Gotcha/common/exception/exceptionCode/ExceptionCode.java
+++ src/main/java/Gotcha/common/exception/exceptionCode/ExceptionCode.java
@@ -3,6 +3,7 @@ package Gotcha.common.exception.exceptionCode;
 import org.springframework.http.HttpStatus;
 
 public interface ExceptionCode {
+    String getCode();
     HttpStatus getStatus();
     String getMessage();
 }
--- src/main/java/Gotcha/domain/notification/exception/NotificationExceptionCode.java
+++ src/main/java/Gotcha/domain/notification/exception/NotificationExceptionCode.java
@@ -9,15 +9,21 @@ import org.springframework.http.HttpStatus;
 @ToString
 public enum NotificationExceptionCode implements ExceptionCode {
 
-    UNAUTHORIZED_ACTION(HttpStatus.FORBIDDEN, "권한이 없습니다."),
-    NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 공지사항입니다.");
+    UNAUTHORIZED_ACTION(HttpStatus.FORBIDDEN, "NOTI-403-001", "권한이 없습니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "NOTI-404-001", "존재하지 않는 공지사항입니다.");
 
     private final HttpStatus status;
+    private final String code;
     private final String message;
 
     @Override
     public HttpStatus getStatus() {
         return status;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
     }
 
     @Override
