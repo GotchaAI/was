c0e98461e1e740c813e5c14478f3ee01d5bd8b6d
brothergiven <128305393+brothergiven@users.noreply.github.com>
2025-04-27 15:08:34 +0900

    refact: QnA 예외코드 수정 (#27)

--- src/main/java/Gotcha/domain/inquiry/exception/InquiryExceptionCode.java
+++ src/main/java/Gotcha/domain/inquiry/exception/InquiryExceptionCode.java
@@ -7,11 +7,12 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum InquiryExceptionCode implements ExceptionCode {
 
-    INVALID_INQUIRYID(HttpStatus.NOT_FOUND, "존재하지 않는 QnA 입니다."),
-    UNAUTHORIZED_ACTION(HttpStatus.FORBIDDEN, "권한이 없습니다."),
-    ALREADY_SOLVED(HttpStatus.CONFLICT, "이미 답변이 작성되었습니다.");
+    INVALID_INQUIRYID(HttpStatus.NOT_FOUND, "QNA-404-001", "존재하지 않는 QnA 입니다."),
+    UNAUTHORIZED_ACTION(HttpStatus.FORBIDDEN, "QNA-403-001","권한이 없습니다."),
+    ALREADY_SOLVED(HttpStatus.CONFLICT, "QNA-409-001", "이미 답변이 작성되었습니다.");
 
     private final HttpStatus status;
+    private final String code;
     private final String message;
 
     @Override
@@ -20,7 +21,14 @@ public enum InquiryExceptionCode implements ExceptionCode {
     }
 
     @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
     public String getMessage() {
         return message;
     }
+
+
 }
