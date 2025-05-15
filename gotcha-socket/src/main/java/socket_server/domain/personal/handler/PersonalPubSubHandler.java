09939eaf6e9704c6d7e2897fcbf77ba93eaac3ae
minju <minju9711@naver.com>
2025-05-16 03:11:38 +0900

    [feat] 개인메시지 전송을 위한 핸들러 클래스 추가

---
+++ gotcha-socket/src/main/java/socket_server/domain/personal/handler/PersonalPubSubHandler.java
@@ -0,0 +1,35 @@
+package socket_server.domain.personal.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+import socket_server.common.config.RedisMessage;
+import socket_server.common.listener.PubSubHandler;
+import socket_server.domain.room.model.RoomMetadata;
+
+import static socket_server.common.constants.WebSocketConstants.PERSONAL_ROOM_CREATE_RESPONSE;
+
+@Slf4j
+@Service
+@Qualifier("PersonalPubSubHandler")
+public class PersonalPubSubHandler extends PubSubHandler {
+
+    public PersonalPubSubHandler(SimpMessagingTemplate messagingTemplate) {
+        super(messagingTemplate);
+    }
+
+    @Override
+    protected void initHandlers() {
+        handlers.put(PERSONAL_ROOM_CREATE_RESPONSE, this::roomCreateResponse);
+    }
+
+    private void roomCreateResponse (String channel, Object object) {
+        RedisMessage redisMessage = (RedisMessage) object;
+        RoomMetadata roomMetadata = convertMessageToDto(redisMessage.payload(), RoomMetadata.class);
+        messagingTemplate.convertAndSend(channel, roomMetadata);
+    }
+
+
+}
+
