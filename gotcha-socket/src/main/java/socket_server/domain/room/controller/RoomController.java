1f0ce300e0e1cceee7a72495d5dcb1ed497a95a3
minju <minju9711@naver.com>
2025-05-16 03:25:49 +0900

    [feat] 방 생성 api Controller 추가

--- gotcha-socket/src/main/java/socket_server/domain/room/controller/RoomController.java
+++ gotcha-socket/src/main/java/socket_server/domain/room/controller/RoomController.java
@@ -1,4 +1,49 @@
 package socket_server.domain.room.controller;
 
+import gotcha_common.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Controller;
+import socket_server.common.config.RedisMessage;
+import socket_server.common.exception.room.RoomExceptionCode;
+import socket_server.domain.room.dto.CreateRoomRequest;
+import socket_server.domain.room.model.RoomMetadata;
+import socket_server.domain.room.service.RoomService;
+import socket_server.domain.room.service.RoomUserService;
+
+import java.security.Principal;
+
+import static socket_server.common.constants.WebSocketConstants.PERSONAL_ROOM_CREATE_RESPONSE;
+import static socket_server.common.constants.WebSocketConstants.ROOM_CREATE_INFO;
+
+@Slf4j
+@Controller
+@MessageMapping("/room")
+@RequiredArgsConstructor
 public class RoomController {
+    private final RoomService roomService;
+    private final RoomUserService roomUserService;
+    private final RedisTemplate<String, Object> objectRedisTemplate;
+
+    @MessageMapping("/create")
+    public void createRoom(@Payload CreateRoomRequest request, Principal principal) {
+        String userId = principal.getName();
+        //request 수동 유효성 검사해주는 코드 필요함.
+
+        if (roomUserService.getCurrentRoomOfUser(userId).isPresent()) {
+            throw new CustomException(RoomExceptionCode.USER_ALREADY_IN_ANOTHER_ROOM);
+        }
+
+        String roomId = roomService.createRoom(request, userId);
+        RoomMetadata metadata = roomService.getRoomInfo(roomId);
+
+        objectRedisTemplate.convertAndSend(ROOM_CREATE_INFO,
+                new RedisMessage(userId, ROOM_CREATE_INFO, metadata)); //방 목록 생성 브로드 캐스트 용
+        objectRedisTemplate.convertAndSend(PERSONAL_ROOM_CREATE_RESPONSE,
+                new RedisMessage(userId, PERSONAL_ROOM_CREATE_RESPONSE, metadata)); //본인의 대기방 생성 확인 용
+    }
+
 }
