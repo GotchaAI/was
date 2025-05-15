8ece057738ac9d612dbd58749f5d8a1036caa213
minju <minju9711@naver.com>
2025-05-16 03:29:17 +0900

    [feat] 클라이언트와 소통 시 데이터 탐을 dto 클래스 추가

--- gotcha-socket/src/main/java/socket_server/domain/room/model/RoomMetadata.java
+++ gotcha-socket/src/main/java/socket_server/domain/room/model/RoomMetadata.java
@@ -1,4 +1,39 @@
 package socket_server.domain.room.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RoomMetadata {
+    private String id;
+    private String title;
+    private String owner;
+    private boolean hasPassword;
+    private String password;
+    private int max;
+    private int min;
+    private String aiLevel;
+    private String gameMode;
+
+
+    public static RoomMetadata fromRedisMap(String id, Map<Object, Object> map) {
+        RoomMetadata metadata = new RoomMetadata();
+        metadata.id = id;
+        metadata.title = (String) map.getOrDefault("title", "");
+        metadata.owner = (String) map.getOrDefault("owner", "");
+        metadata.hasPassword = Boolean.parseBoolean((String) map.getOrDefault("hasPassword", "false"));
+        metadata.password = (String) map.getOrDefault("password", "");
+        metadata.max = Integer.parseInt((String) map.getOrDefault("max", "0"));
+        metadata.min = Integer.parseInt((String) map.getOrDefault("min", "0"));
+        metadata.aiLevel = (String) map.getOrDefault("aiLevel", "NORMAL");
+        metadata.gameMode = (String) map.getOrDefault("gameMode", "STANDARD");
+        return metadata;
+    }
 }
