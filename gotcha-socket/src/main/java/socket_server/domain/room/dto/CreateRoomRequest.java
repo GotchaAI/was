e7eb6e122e9c5de836a038cdfd4f6d5041c2b2a6
minju <minju9711@naver.com>
2025-05-17 04:55:23 +0900

    feat: 방 생성 요청 시 사용할 dto에 @Valid 애노테이션 추가

--- gotcha-socket/src/main/java/socket_server/domain/room/dto/CreateRoomRequest.java
+++ gotcha-socket/src/main/java/socket_server/domain/room/dto/CreateRoomRequest.java
@@ -1,13 +1,16 @@
 package socket_server.domain.room.dto;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import socket_server.domain.game.enumType.AiMode;
 import socket_server.domain.game.enumType.GameMode;
 
 public record CreateRoomRequest(
-        String title,
+        @NotBlank String title,
         boolean hasPassword,
-        String password,
-        AiMode aimode,
-        GameMode gameMode
+        @Size(min = 4, max = 20) String password,
+        @NotNull AiMode aimode,
+        @NotNull GameMode gameMode
 ) {}
