9b503b340d6e5cd2922bc11f3ba0cb7930f1cb2f
minju <minju9711@naver.com>
2025-05-18 01:47:34 +0900

    refactor: SecurityUserDetails로 사용자 인가 객체 받아오도록 변경 refactor: 컨트롤러 내부의 비즈니스 로직을 서비스 계층으로 넣음.

--- gotcha-socket/src/main/java/socket_server/domain/room/controller/RoomController.java
+++ gotcha-socket/src/main/java/socket_server/domain/room/controller/RoomController.java
@@ -1,6 +1,6 @@
 package socket_server.domain.room.controller;
 
-import gotcha_common.exception.CustomException;
+import gotcha_domain.auth.SecurityUserDetails;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,15 +8,12 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import socket_server.common.config.RedisMessage;
-import socket_server.common.exception.room.RoomExceptionCode;
 import socket_server.domain.room.dto.CreateRoomRequest;
 import socket_server.domain.room.model.RoomMetadata;
 import socket_server.domain.room.service.RoomService;
 import socket_server.domain.room.service.RoomUserService;
-
 
 import static socket_server.common.constants.WebSocketConstants.PERSONAL_ROOM_CREATE_RESPONSE;
 import static socket_server.common.constants.WebSocketConstants.ROOM_CREATE_INFO;
@@ -31,15 +28,11 @@ public class RoomController {
     private final RedisTemplate<String, Object> objectRedisTemplate;
 
     @MessageMapping("/create")
-    public void createRoom(@Valid @Payload CreateRoomRequest request, @AuthenticationPrincipal UserDetails user) {
-        String userId = user.getUsername();
+    public void createRoom(@Valid @Payload CreateRoomRequest request, @AuthenticationPrincipal SecurityUserDetails userDetails) {
+        String userId = userDetails.getUsername();
 
-        if (roomUserService.getCurrentRoomOfUser(userId).isPresent()) {
-            throw new CustomException(RoomExceptionCode.USER_ALREADY_IN_ANOTHER_ROOM);
-        }
-
-        String roomId = roomService.createRoom(request, userId);
-        RoomMetadata metadata = roomService.getRoomInfo(roomId);
+        roomUserService.checkUserNotInAnyRoom(userId);
+        RoomMetadata metadata = roomService.createRoom(request, userId);
 
         objectRedisTemplate.convertAndSend(ROOM_CREATE_INFO,
                 new RedisMessage(userId, ROOM_CREATE_INFO, metadata)); //방 목록 생성 브로드 캐스트 용
