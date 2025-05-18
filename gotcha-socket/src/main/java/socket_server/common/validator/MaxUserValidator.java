0dc23317b436562f81a7e4ac1591a877d7d8e9f3
minju <minju9711@naver.com>
2025-05-18 18:25:33 +0900

    remove: 기존 gotcha 모듈에서 gotcha-socket 모듈로 이동

---
+++ gotcha-socket/src/main/java/socket_server/common/validator/MaxUserValidator.java
@@ -0,0 +1,22 @@
+package socket_server.common.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import socket_server.domain.game.enumType.GameMode;
+import socket_server.domain.room.dto.CreateRoomRequest;
+
+public class MaxUserValidator implements ConstraintValidator<ValidMaxUser, CreateRoomRequest> {
+
+    @Override
+    public boolean isValid(CreateRoomRequest req, ConstraintValidatorContext context) {
+        if (req == null || req.gameMode() == null || req.maxUser() == null) {
+            return true;
+        }
+
+        GameMode gameMode = req.gameMode();
+        int maxUser = req.maxUser();
+
+        return maxUser >= gameMode.getMinPlayers() && maxUser <= gameMode.getMaxPlayers();
+    }
+
+}
