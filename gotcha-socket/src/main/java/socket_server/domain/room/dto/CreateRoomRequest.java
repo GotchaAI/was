20f92f6b2d3366edbeac180d0f1e4e9896db3401
minju <minju9711@naver.com>
2025-05-18 18:28:01 +0900

    refactor: 기존 방생성 요청 dto 검증 로직 추가 add: 라운드 수 필드 추가

--- gotcha-socket/src/main/java/socket_server/domain/room/dto/CreateRoomRequest.java
+++ gotcha-socket/src/main/java/socket_server/domain/room/dto/CreateRoomRequest.java
@@ -1,16 +1,31 @@
 package socket_server.domain.room.dto;
 
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
+import socket_server.common.validator.ValidMaxUser;
 import socket_server.domain.game.enumType.AiMode;
 import socket_server.domain.game.enumType.GameMode;
 
+@ValidMaxUser
 public record CreateRoomRequest(
-        @NotBlank String title,
+        @NotBlank(message = "제목은 필수 입력입니다.")
+        String title,
+
+        @NotNull(message = "최대 인원수는 필수 입력입니다.")
+        Integer maxUser,
+
+        @NotNull
         boolean hasPassword,
-        @Size(min = 4, max = 20) String password,
-        @NotNull AiMode aimode,
-        @NotNull GameMode gameMode
+
+        @Pattern(regexp = "^[0-9]{4}$")
+        String password,
+
+        @NotNull (message = "인공지능 난이도는 필수 입력입니다.")
+        AiMode aimode,
+
+        @NotNull(message = "게임 유형은 필수 입력입니다.")
+        GameMode gameMode,
+
+        @Min(1) @Max(5)
+        int roundCount
 ) {}
