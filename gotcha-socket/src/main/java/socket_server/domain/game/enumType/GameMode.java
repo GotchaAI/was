f74a82f6a257123b67ec73cfea3d70727fc1c725
minju <minju9711@naver.com>
2025-05-18 22:38:43 +0900

    fix: 묘묘를 속여라 최대 플레이어 수 수정

--- gotcha-socket/src/main/java/socket_server/domain/game/enumType/GameMode.java
+++ gotcha-socket/src/main/java/socket_server/domain/game/enumType/GameMode.java
@@ -1,7 +1,7 @@
 package socket_server.domain.game.enumType;
 
 public enum GameMode {
-    TRICK_MYOMYO("묘묘를 속여라", 2, 8),
+    TRICK_MYOMYO("묘묘를 속여라", 2, 2),
     LULU_ART_EXAM("루루의 미대입시", 1, 1);
 
     private final int minPlayers;
