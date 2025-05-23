1f28cda68c6091b2454226e27aebed10b15834ea
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-05-23 14:26:02 +0900

    refact: 불필요 메서드 삭제

--- gotcha-socket/src/main/java/socket_server/domain/chat/dto/ChatMessage.java
+++ gotcha-socket/src/main/java/socket_server/domain/chat/dto/ChatMessage.java
@@ -1,10 +1,5 @@
 package socket_server.domain.chat.dto;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-
 import java.time.LocalDateTime;
 
 public record ChatMessage(
@@ -13,14 +8,4 @@ public record ChatMessage(
         ChatType type,
         LocalDateTime sentAt
 ) {
-    public String toJson() {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.registerModule(new JavaTimeModule()); // 필수
-            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // 가독성 좋게
-            return objectMapper.writeValueAsString(this);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
