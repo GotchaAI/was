71dd7ad45f8295858c0f7fb4eea734058af0a43c
brothergiven <dung3740@gmail.com>
2025-05-24 22:59:47 +0900

    feat: 예외 코드 수정

--- gotcha-socket/src/main/java/socket_server/common/exception/game/GameExceptionCode.java
+++ gotcha-socket/src/main/java/socket_server/common/exception/game/GameExceptionCode.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum GameExceptionCode implements ExceptionCode {
 
-    INVALID_GAME_ID(HttpStatus.BAD_REQUEST, "GAME_400_001", "유효하지 않은 고유 방 코드 입니다."),
-    GAME_ID_EXHAUSTED(HttpStatus.SERVICE_UNAVAILABLE, "GLOBAL-503-001", "사용 가능한 게임 코드가 모두 소진되었습니다. 서버 팀에 문의해주세요.");
+    INVALID_GAME_ID(HttpStatus.BAD_REQUEST, "GAME-400-001", "유효하지 않은 고유 방 코드 입니다."),
+    GAME_ID_EXHAUSTED(HttpStatus.SERVICE_UNAVAILABLE, "GAME-503-001", "사용 가능한 게임 코드가 모두 소진되었습니다. 서버 팀에 문의해주세요."),
+    INVALID_EVENT_TYPE(HttpStatus.BAD_REQUEST, "GAME-400-002", "유효하지 않은 이벤트 타입입니다.");
+
 
     private final HttpStatus status;
     private final String code;
