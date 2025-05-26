014245694bb9625da737062d3dc7d9c348273b6c
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-05-26 21:34:38 +0900

    refact: 어노테이션 오류 수정

--- gotcha/src/main/java/Gotcha/domain/ranking/controller/RankingController.java
+++ gotcha/src/main/java/Gotcha/domain/ranking/controller/RankingController.java
@@ -3,7 +3,6 @@ package Gotcha.domain.ranking.controller;
 import Gotcha.domain.ranking.api.RankingApi;
 import Gotcha.domain.ranking.service.RankingRedisService;
 import gotcha_domain.auth.SecurityUserDetails;
-import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RankingController implements RankingApi {
     private final RankingRedisService rankingRedisService;
 
-    @Operation
+    @Override
     @GetMapping()
     public ResponseEntity<?> getUserRankingPage(@RequestParam(defaultValue = "0") int page) {
         return ResponseEntity.ok(rankingRedisService.getUserRankingPage(page));
