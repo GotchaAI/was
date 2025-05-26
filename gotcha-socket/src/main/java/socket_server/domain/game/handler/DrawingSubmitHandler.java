41b99e15c210cd183427b5b165b0e6fdab206855
brothergiven <dung3740@gmail.com>
2025-05-26 17:27:29 +0900

    feat: drawing 제출 이벤트 handler 클래스 작성

---
+++ gotcha-socket/src/main/java/socket_server/domain/game/handler/DrawingSubmitHandler.java
@@ -0,0 +1,30 @@
+package socket_server.domain.game.handler;
+
+import gotcha_domain.auth.SecurityUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import socket_server.common.util.JsonSerializer;
+import socket_server.domain.game.dto.DrawingSubmitReq;
+import socket_server.domain.game.dto.GameEventType;
+import socket_server.domain.game.dto.GameReq;
+import socket_server.domain.game.service.GameService;
+
+@Component
+@RequiredArgsConstructor
+public class DrawingSubmitHandler implements GameEventHandler {
+
+    private final GameService gameService;
+    private final JsonSerializer jsonSerializer;
+
+    @Override
+    public GameEventType getEventType() {
+        return GameEventType.DRAWING_SUBMIT;
+    }
+
+    @Override
+    public void handle(String roomId, SecurityUserDetails userDetails, GameReq request) {
+        DrawingSubmitReq drawingSubmit = jsonSerializer.deserialize(request.data(), DrawingSubmitReq.class);
+        gameService.submitDrawing(roomId, userDetails.getUuid(), drawingSubmit.imageData());
+    }
+
+}
