0044dd53d5edcf7d44517f18bc108184cb859e7c
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-05-26 21:25:50 +0900

    feat: 자신의 랭킹 반환 로직 작성

--- gotcha/src/main/java/Gotcha/domain/ranking/controller/RankingController.java
+++ gotcha/src/main/java/Gotcha/domain/ranking/controller/RankingController.java
@@ -1,8 +1,10 @@
 package Gotcha.domain.ranking.controller;
 
 import Gotcha.domain.ranking.service.RankingRedisService;
+import gotcha_domain.auth.SecurityUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,5 +19,10 @@ public class RankingController {
     @GetMapping()
     public ResponseEntity<?> getUserRankingPage(@RequestParam(defaultValue = "0") int page) {
         return ResponseEntity.ok(rankingRedisService.getUserRankingPage(page));
+    }
+
+    @GetMapping("/mine")
+    public ResponseEntity<?> getMyRanking(@AuthenticationPrincipal SecurityUserDetails userDetails) {
+        return ResponseEntity.ok(rankingRedisService.getUserRank(userDetails.getId()));
     }
 }
---
+++ gotcha/src/main/java/Gotcha/domain/ranking/exception/RankingExceptionCode.java
@@ -0,0 +1,29 @@
+package Gotcha.domain.ranking.exception;
+
+import gotcha_common.exception.exceptionCode.ExceptionCode;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+public enum RankingExceptionCode implements ExceptionCode {
+    RANKING_NOT_FOUND(HttpStatus.NOT_FOUND, "RANK-404-001", "랭킹에 등록되지 않은 사용자입니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
