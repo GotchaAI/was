b467206a97129e324756d63ece042f861e770db9
brothergiven <dung3740@gmail.com>
2025-05-27 23:44:37 +0900

    feat: AI 추측 시작 메시지 받는 기능 연결

--- gotcha-socket/src/main/java/socket_server/domain/game/service/AIClientService.java
+++ gotcha-socket/src/main/java/socket_server/domain/game/service/AIClientService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import socket_server.domain.game.dto.AIGameStartReq;
+import socket_server.domain.game.dto.AIGuessStartReq;
 import socket_server.domain.game.dto.AIRoundStartReq;
 
 @Service
@@ -28,6 +29,15 @@ public class AIClientService {
     public String getRoundStartMessage(String roomId, AIRoundStartReq request){
         return webClient.post()
                 .uri(AI_SERVER_BASE_URL + "chat/" + roomId + "/round/start")
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+    public String getGuessStartMessage(String roomId, AIGuessStartReq request){
+        return webClient.post()
+                .uri(AI_SERVER_BASE_URL + "chat/" + roomId + "/guess/start")
                 .bodyValue(request)
                 .retrieve()
                 .bodyToMono(String.class)
