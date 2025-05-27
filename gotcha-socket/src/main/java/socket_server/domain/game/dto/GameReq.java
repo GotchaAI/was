3efce94d05cc6ed2855b36a96b270730dda9a6e9
brothergiven <dung3740@gmail.com>
2025-05-27 20:35:07 +0900

    feat: 필드 명 수정

--- gotcha-socket/src/main/java/socket_server/domain/game/dto/GameReq.java
+++ gotcha-socket/src/main/java/socket_server/domain/game/dto/GameReq.java
@@ -6,7 +6,7 @@ import socket_server.domain.game.enumType.GameEventType;
  * 게임 이벤트 요청 들어올 때 클라이언트에서 넘겨줄 DTO
  */
 public record GameReq(
-        GameEventType gameEventType,
+        GameEventType eventType,
         Object data
 ) {
 }
