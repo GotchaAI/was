f06d8cb9959217606df568d296c5ec9ea3c1a813
minju <minju9711@naver.com>
2025-05-30 08:03:09 +0900

    feat: RoomEventType에 crate, join 추가

--- gotcha-socket/src/main/java/socket_server/domain/room/model/RoomEventType.java
+++ gotcha-socket/src/main/java/socket_server/domain/room/model/RoomEventType.java
@@ -1,5 +1,5 @@
 package socket_server.domain.room.model;
 
 public enum RoomEventType {
-    CHAT, READY, UNREADY, EXIT, START, UPDATE, DELETE
+    CHAT, READY, UNREADY, EXIT, START, UPDATE, DELETE, CREATE, JOIN
 }
