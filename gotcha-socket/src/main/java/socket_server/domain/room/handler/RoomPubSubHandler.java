72619aeecc8f7753b5d7d289b6bf824ac589de4b
minju <minju9711@naver.com>
2025-05-30 08:04:42 +0900

    feat: roomHandelr에서 로비에 필요한 함수 모두 이동

--- gotcha-socket/src/main/java/socket_server/domain/room/handler/RoomPubSubHandler.java
+++ gotcha-socket/src/main/java/socket_server/domain/room/handler/RoomPubSubHandler.java
@@ -5,19 +5,17 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import socket_server.common.config.RedisMessage;
+import socket_server.common.exception.ErrorType;
 import socket_server.common.listener.PubSubHandler;
 import socket_server.common.util.JsonSerializer;
 import socket_server.domain.room.dto.EventRes;
 
-import static socket_server.common.constants.WebSocketConstants.ROOM_LIST_EVENT;
 import static socket_server.common.constants.WebSocketConstants.ROOM_EVENT;
-import static socket_server.common.constants.WebSocketConstants.ROOM_OWNER_CREATE_INFO;
 
 @Slf4j
 @Service
 @Qualifier("roomPubSubHandler")
 public class RoomPubSubHandler extends PubSubHandler {
-
 
     public RoomPubSubHandler(SimpMessagingTemplate messagingTemplate, JsonSerializer jsonSerializer) {
         super(messagingTemplate, jsonSerializer);
@@ -25,14 +23,12 @@ public class RoomPubSubHandler extends PubSubHandler {
 
     @Override
     protected void initHandlers() {
-        handlers.put(ROOM_LIST_EVENT, this::handleEventResMessage);
-        handlers.put(ROOM_OWNER_CREATE_INFO, this::handleEventResMessage);
         handlers.put(ROOM_EVENT, this::handleEventResMessage);
     }
 
     private void handleEventResMessage(String channel, Object object) {
         RedisMessage redisMessage = (RedisMessage) object;
-        EventRes eventRes = jsonSerializer.deserialize(redisMessage.payload(), EventRes.class);
+        EventRes eventRes = jsonSerializer.deserialize(redisMessage.payload(), EventRes.class, ErrorType.ROOM);
         messagingTemplate.convertAndSend(channel, eventRes);
     }
 
