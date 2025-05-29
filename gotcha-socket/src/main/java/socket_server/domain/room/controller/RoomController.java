0c523b76f14c77c9f2a683313d3a21ce8bf5b33c
minju <minju9711@naver.com>
2025-05-30 08:04:00 +0900

    refactor: reqDto 유효성 검사를 애노테이션에서 수동 검사로 변경

--- gotcha-socket/src/main/java/socket_server/domain/room/controller/RoomController.java
+++ gotcha-socket/src/main/java/socket_server/domain/room/controller/RoomController.java
@@ -1,7 +1,6 @@
 package socket_server.domain.room.controller;
 
 import gotcha_domain.auth.SecurityUserDetails;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -9,29 +8,25 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import socket_server.domain.room.dto.CreateRoomRequest;
+import socket_server.common.exception.ErrorType;
+import socket_server.common.validator.SocketFieldValidator;
 import socket_server.domain.room.dto.RoomReq;
 import socket_server.domain.room.handler.RoomEventDispatcher;
-import socket_server.domain.room.service.RoomService;
 
 @Slf4j
 @Controller
 @MessageMapping("/room")
 @RequiredArgsConstructor
 public class RoomController {
-
-    private final RoomService roomService;
+    private final SocketFieldValidator fieldValidator;
     private final RoomEventDispatcher dispatcher;
-
-    @MessageMapping("/create")
-    public void createRoom(@Valid @Payload CreateRoomRequest request, @AuthenticationPrincipal SecurityUserDetails userDetails) {
-        roomService.handleCreateRoom(request, userDetails);
-    }
 
     @MessageMapping("/{roomId}")
     public void room(@DestinationVariable String roomId,
-                     @Valid @Payload RoomReq request,
+                     @Payload RoomReq request,
                      @AuthenticationPrincipal SecurityUserDetails userDetails) {
+        fieldValidator.validateOrThrow(request, ErrorType.ROOM);
         dispatcher.dispatch(request, roomId, userDetails);
     }
+
 }
