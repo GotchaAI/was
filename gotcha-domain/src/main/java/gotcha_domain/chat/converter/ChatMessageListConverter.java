ff9999cffaf4e087344fd753392476abf5139d27
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-06-01 16:56:25 +0900

    refact: LocalDateTime 직렬화 오류 수정

--- gotcha-domain/src/main/java/gotcha_domain/chat/converter/ChatMessageListConverter.java
+++ gotcha-domain/src/main/java/gotcha_domain/chat/converter/ChatMessageListConverter.java
@@ -2,6 +2,8 @@ package gotcha_domain.chat.converter;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import gotcha_domain.chat.ChatMessage;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
@@ -11,7 +13,10 @@ import java.util.List;
 @Converter
 public class ChatMessageListConverter implements AttributeConverter<List<ChatMessage>, String> {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
 
     @Override
     public String convertToDatabaseColumn(List<ChatMessage> chatMessages) {
