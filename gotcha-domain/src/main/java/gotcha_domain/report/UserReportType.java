4e120c5e30a0d2e93d77bef966f9ef51b7a946e1
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-06-01 16:41:13 +0900

    feat: 사용자 신고 타입 작성

--- gotcha-domain/src/main/java/gotcha_domain/report/UserReportType.java
+++ gotcha-domain/src/main/java/gotcha_domain/report/UserReportType.java
@@ -1,4 +1,9 @@
 package gotcha_domain.report;
 
 public enum UserReportType {
+    BAD_LANGUAGE, // 욕설
+    HATE_SPEECH, // 혐오 발언
+    INAPPROPRIATE_NAME, // 불쾌감을 주거나 부적절한 이름
+    SPAM, // 도배
+    OTHER // 기타
 }
