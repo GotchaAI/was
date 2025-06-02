c4f06eea194239845c7f1710d2819ca6f3d076c0
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-06-02 22:53:31 +0900

    feat: 대기방 상세 정보 dto 명 변경

--- gotcha-socket/src/main/java/socket_server/domain/lobby/dto/RoomDetailRes.java
+++ gotcha-socket/src/main/java/socket_server/domain/lobby/dto/RoomDetailRes.java
@@ -1,12 +1,12 @@
 package socket_server.domain.lobby.dto;
 
-import socket_server.domain.room.model.RoomMetadata;
+import socket_server.domain.room.dto.RoomInfoRes;
 import socket_server.domain.room.model.RoomUserInfo;
 
 import java.util.List;
 
-public record RoomJoinRes(
-        RoomMetadata roomMetadata,
+public record RoomDetailRes(
+        RoomInfoRes roomInfo,
         List<RoomUserInfo> userInfos
 ) {
 }
