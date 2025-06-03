0324908a15c57ca19d3416e8f6ba8ef9bd1ff553
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-06-03 21:39:51 +0900

    docs: 대기방 Swagger 수정

--- gotcha/src/main/java/Gotcha/domain/room/api/RoomApi.java
+++ gotcha/src/main/java/Gotcha/domain/room/api/RoomApi.java
@@ -1,5 +1,6 @@
 package Gotcha.domain.room.api;
 
+import socket_server.domain.room.dto.RoomListReq;
 import gotcha_domain.auth.SecurityUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -10,6 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "[대기방 API]", description = "대기방 관련 API")
 public interface RoomApi {
@@ -19,19 +21,31 @@ public interface RoomApi {
                     content = @Content(mediaType = "application/json", examples = {
                             @ExampleObject(value = """
                                     [
-                                        {
-                                            "roomId": "5876",
-                                            "title": "고양이의 비밀방",
-                                            "owner": "관리자",
-                                            "hasPassword": true,
-                                            "maxUser": 2,
-                                            "currentUser": 2
-                                        }
+                                         {
+                                             "roomId": "7390",
+                                             "title": "고양이를 속여보자",
+                                             "owner": "관리자",
+                                             "gameType": "TRICK_MYOMYO",
+                                             "difficulty": "BASIC",
+                                             "hasPassword": false,
+                                             "maxUser": 2,
+                                             "currentUser": 1
+                                         },
+                                         {
+                                             "roomId": "7705",
+                                             "title": "고양이의 비밀방",
+                                             "owner": "테스터",
+                                             "gameType": "TRICK_MYOMYO",
+                                             "difficulty": "ADVANCED",
+                                             "hasPassword": false,
+                                             "maxUser": 2,
+                                             "currentUser": 1
+                                         }
                                     ]
                                     """)
                     }))
     })
-    ResponseEntity<?> getRoomSummaries();
+    ResponseEntity<?> getRoomSummaries(@RequestBody RoomListReq roomListReq);
 
     @Operation(summary = "대기방 상세 정보 조회", description = "대기방 입장 시 초기 상세 정보 조회 API")
     @ApiResponses({
