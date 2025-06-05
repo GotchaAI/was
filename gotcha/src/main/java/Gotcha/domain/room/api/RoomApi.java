cdba8f39d99df4314e9c60db857d5fd4e66f7e9c
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-06-05 11:58:21 +0900

    docs: 대기방 목록 요청 방식 변경

--- gotcha/src/main/java/Gotcha/domain/room/api/RoomApi.java
+++ gotcha/src/main/java/Gotcha/domain/room/api/RoomApi.java
@@ -1,6 +1,5 @@
 package Gotcha.domain.room.api;
 
-import socket_server.domain.room.dto.RoomListReq;
 import gotcha_domain.auth.SecurityUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -8,10 +7,12 @@ import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import socket_server.domain.room.dto.RoomListReq;
 
 @Tag(name = "[대기방 API]", description = "대기방 관련 API")
 public interface RoomApi {
@@ -45,7 +46,7 @@ public interface RoomApi {
                                     """)
                     }))
     })
-    ResponseEntity<?> getRoomSummaries(@RequestBody RoomListReq roomListReq);
+    ResponseEntity<?> getRoomSummaries(@ParameterObject @Valid RoomListReq roomListReq);
 
     @Operation(summary = "대기방 상세 정보 조회", description = "대기방 입장 시 초기 상세 정보 조회 API")
     @ApiResponses({
