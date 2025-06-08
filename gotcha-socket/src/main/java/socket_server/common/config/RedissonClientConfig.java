ca8dd4633eeec3421d0f5834907af80d93094fe5
brothergiven <dung3740@gmail.com>
2025-06-09 00:35:04 +0900

    feat: Redisson Bean 등록

---
+++ gotcha-socket/src/main/java/socket_server/common/config/RedissonClientConfig.java
@@ -0,0 +1,25 @@
+package socket_server.common.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.spring.data.connection.RedissonConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonClientConfig {
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://localhost:6379");
+        return Redisson.create(config);
+    }
+
+    @Bean
+    public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redissonClient){
+        return new RedissonConnectionFactory(redissonClient);
+    }
+
+}
