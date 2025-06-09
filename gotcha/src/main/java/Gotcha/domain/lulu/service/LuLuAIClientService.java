32dd71a53e53d43b9d8066ed26be2583906a4b80
brothergiven <dung3740@gmail.com>
2025-06-10 07:46:55 +0900

    feat: 이미지 Captioning 추가

--- gotcha/src/main/java/Gotcha/domain/lulu/service/LuLuAIClientService.java
+++ gotcha/src/main/java/Gotcha/domain/lulu/service/LuLuAIClientService.java
@@ -11,7 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -40,7 +43,7 @@ public class LuLuAIClientService {
             if (res == null || res.gameId() == null)
                 throw new CustomException(LuLuExceptionCode.AI_SERVER_FORMAT_ERROR);
             return res;
-        } catch (Exception e) {
+        } catch (WebClientResponseException e) {
             log.error("LuLuAIClientService startGame error", e);
             throw new CustomException(LuLuExceptionCode.AI_SERVER_FORMAT_ERROR);
         }
@@ -61,10 +64,12 @@ public class LuLuAIClientService {
                                     new CustomException(LuLuExceptionCode.AI_SERVER_ERROR))))
                     .bodyToMono(TaskStartRes.class)
                     .block();
-            if (res == null || res.keyword() == null || res.situation() == null)
+            if (res == null || res.keyword() == null || res.situation() == null){
+                log.error(String.valueOf(res));
                 throw new CustomException(LuLuExceptionCode.AI_SERVER_FORMAT_ERROR);
+            }
             return res;
-        } catch (Exception e) {
+        } catch (WebClientResponseException e) {
             log.error("LuLuAIClientService generateTask error", e);
             throw new CustomException(LuLuExceptionCode.AI_SERVER_FORMAT_ERROR);
         }
@@ -75,10 +80,16 @@ public class LuLuAIClientService {
      * 루루 키워드 평가
      */
     public TaskEvalRes evaluateTask(String gameId, TaskEvalReq taskEvalReq){
+        String caption = getImageCaption(taskEvalReq.imageURL());
+        return requestEvaluate(gameId, caption);
+    }
+
+    private TaskEvalRes requestEvaluate(String gameId, String imageCaption){
+        Map<String, String> body = Map.of("description", imageCaption);
         try {
             TaskEvalRes res = webClient.post()
-                    .uri(AI_SERVER_BASE_URL + "lulu/task" + gameId)
-                    .bodyValue(taskEvalReq)
+                    .uri(AI_SERVER_BASE_URL + "lulu/task/" + gameId)
+                    .bodyValue(body)
                     .retrieve()
                     .onStatus(httpStatusCode -> httpStatusCode.is4xxClientError() || httpStatusCode.is5xxServerError(
                             )
@@ -86,15 +97,35 @@ public class LuLuAIClientService {
                                     new CustomException(LuLuExceptionCode.AI_SERVER_ERROR))))
                     .bodyToMono(TaskEvalRes.class)
                     .block();
-            if (res == null || res.score() == null || res.feedback() == null || res.task() == null)
+            if (res == null || res.score() == null || res.feedback() == null || res.gameId() == null)
                 throw new CustomException(LuLuExceptionCode.AI_SERVER_FORMAT_ERROR);
             return res;
-        } catch (Exception e) {
+        } catch (WebClientResponseException e) {
             log.error("LuLuAIClientService evaluateTask error", e);
             throw new CustomException(LuLuExceptionCode.AI_SERVER_FORMAT_ERROR);
         }
     }
 
+
+
+
+
+    /**
+     * 이미지 캡셔닝
+     */
+    public String getImageCaption(String imageUrl){
+        Map<String, String> body = Map.of("imageURL", imageUrl);
+        return webClient.post()
+                .uri(AI_SERVER_BASE_URL + "image/caption")
+                .bodyValue(body)
+                .retrieve()
+                .onStatus(httpStatusCode -> httpStatusCode.is4xxClientError() || httpStatusCode.is5xxServerError(
+                        )
+                        , clientResponse -> clientResponse.bodyToMono(String.class).flatMap(error -> Mono.error(
+                                new CustomException(LuLuExceptionCode.AI_SERVER_ERROR))))
+                .bodyToMono(String.class)
+                .block();
+    }
 
 
 }
