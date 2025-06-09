349f0c54528776785ca63a9a5dd28f323c9aa898
brothergiven <dung3740@gmail.com>
2025-06-10 06:39:42 +0900

    feat: 예외코드 추가

--- gotcha/src/main/java/Gotcha/domain/lulu/exception/LuLuExceptionCode.java
+++ gotcha/src/main/java/Gotcha/domain/lulu/exception/LuLuExceptionCode.java
@@ -6,7 +6,8 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum LuLuExceptionCode implements ExceptionCode {
-    AI_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "LULU-500-001", "AI 서버 내부 오류입니다.");
+    AI_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "LULU-500-001", "AI 서버 내부 오류입니다."),
+    AI_SERVER_FORMAT_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "LULU-500-002", "AI 서버 데이터 파싱에 실패했습니다.");
 
     private final HttpStatus status;
     private final String code;
