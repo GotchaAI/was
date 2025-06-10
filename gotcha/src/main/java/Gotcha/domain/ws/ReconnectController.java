79713129ba52b3983f4318f5793f14647504a583
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-06-10 14:45:40 +0900

    feat: 재접속 API 추가

---
+++ gotcha/src/main/java/Gotcha/domain/ws/ReconnectController.java
@@ -0,0 +1,25 @@
+package Gotcha.domain.ws;
+
+import gotcha_domain.auth.SecurityUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import socket_server.common.util.DisconnectManager;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/ws")
+@RequiredArgsConstructor
+public class ReconnectController {
+    private final DisconnectManager disconnectManager;
+
+    @GetMapping("/reconnect")
+    public ResponseEntity<?> reconnect(@AuthenticationPrincipal SecurityUserDetails userDetails) {
+        String roomId = disconnectManager.cancelDisconnectIfExists(userDetails.getUuid());
+        return ResponseEntity.ok(Map.of("roomId", roomId));
+    }
+}
