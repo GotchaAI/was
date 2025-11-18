1806b71f3959926caced07bba08090490d5b7186
LeeSeunghyeon <50231309+Uralauah@users.noreply.github.com>
2025-11-18 21:58:49 +0900

    feat: 공지사항 타입 별 조회 로직 추가

--- gotcha/src/main/java/Gotcha/domain/notification/repository/NotificationRepository.java
+++ gotcha/src/main/java/Gotcha/domain/notification/repository/NotificationRepository.java
@@ -1,10 +1,12 @@
 package Gotcha.domain.notification.repository;
 
 import gotcha_domain.notification.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.domain.Pageable;
+import gotcha_domain.notification.NotificationType;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     Page<Notification> findByTitleContainingIgnoreCase(String keyword, Pageable pageable);
+    Page<Notification> findByTypeAndTitleContainingIgnoreCase(NotificationType type, String keyword, Pageable pageable);
 }
